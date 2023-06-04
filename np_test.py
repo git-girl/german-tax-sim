215835a10c8d506cdfeacbd01336037e45f26677
git_girl <lotta.stefanova@posteo.de>
2023-06-04 15:57:25 +0200

    switched off of cupy

--- np_test.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import cupy as cp
-import time
-
-asize = 10000000
-nprand = np.random.rand(asize)
-start_time = time.time()
-np.cos(nprand)
-print("--- %s seconds ---" % (time.time() - start_time))
